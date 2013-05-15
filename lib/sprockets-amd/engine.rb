3886fe4729f8242d68844c2044a18647e5f5b9a9
Termina1 <terminal2010@gmail.com>
2013-05-15 13:56:49 +0400

    fix

--- lib/sprockets-amd/engine.rb
+++ lib/sprockets-amd/engine.rb
@@ -2,7 +2,7 @@ require 'sprockets-amd/view_helpers'
 
 module SprocketsAmd
   class Engine < Rails::Engine
-    initializer "sprockets-amd.view_helpers" do |app|
+    initializer "sprockets-amd.setup_helpers" do |app|
       app.config.to_prepare do
         ActionController::Base.send :helper, ViewHelpers
       end
