b387c080763d5f2c1cc2b42c26c7e4ade49e17cd
Termina1 <terminal2010@gmail.com>
2013-05-15 14:43:08 +0400

    no init

--- lib/sprockets-amd/engine.rb
+++ lib/sprockets-amd/engine.rb
@@ -2,10 +2,5 @@ require File.dirname(__FILE__) + '/../../app/helpers/view_helpers'
 
 module SprocketsAmd
   class Engine < Rails::Engine
-    initializer "sprockets-amd.setup_helpers" do |app|
-      app.config.to_prepare do
-        ActionController::Base.send :helper, SprocketsAmd::ViewHelpers
-      end
-    end
   end
 end
