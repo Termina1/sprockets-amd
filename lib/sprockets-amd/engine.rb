430257ad16e1c60a0ac5a4a4d19be24e6e3d10cb
Termina1 <terminal2010@gmail.com>
2013-05-15 12:14:52 +0400

    change way of adding helpers

--- lib/sprockets-amd/engine.rb
+++ lib/sprockets-amd/engine.rb
@@ -3,7 +3,9 @@ require 'sprockets-amd/view_helpers'
 module SprocketsAmd
   class Engine < Rails::Engine
     initializer "sprockets-amd.view_helpers" do
-      ActionView::Base.send :include, ViewHelpers
+      app.config.to_prepare do
+        ActionController::Base.send :helper, ViewHelpers
+      end
     end
   end
 end
